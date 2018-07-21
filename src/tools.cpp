6b6de0062edc2b72def5393bc0ddab6daae2dc5f
Daniel Hunter <officialdanielhunter@gmail.com>
2018-07-21 16:37:02 -0700

    Add calcRMSE from EFK project

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,22 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  if (estimations.size() != ground_truth.size() || estimations.size() == 0) {
+      cout << "Error in CalculateRMSE" << endl;
+      return rmse;
+  }
+
+  // accumulate squared residuals
+  for (unsigned int i = 0; i < estimations.size(); ++i) {
+      VectorXd diff = estimations[i] - ground_truth[i];
+      diff = diff.array() * diff.array();
+      rmse += diff;
+  }
+
+  rmse = rmse / estimations.size(); // calculate the mean
+  rmse = rmse.array().sqrt();
+  return rmse;
 }
